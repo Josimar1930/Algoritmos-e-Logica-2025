d6f112fb4c42b123a0c2581696c3cc0b0759b263
Josimar Almeida <josimar.aalmeida193@gmail.com>
2025-10-12 09:50:51 -0300

    Laços de repetição

--- Repetição de simbolos.py
+++ Repetição de simbolos.py
@@ -1,11 +1,12 @@
 # Repetição de simbolo
-simbol = input("Digite um símbolo (ex: #, *, @): ")
+
+simbolo = input("Digite um símbolo (ex: #, *, @): ")
 
 quantidade = int(input("Digite a quantidade de repetições desejada: "))
 
 print("\n----- Padrão Gerado -----")
 
 for i in range(quantidade):
-  print(simbol)
+  print(simbolo)
 
 print("-------------------------------")
