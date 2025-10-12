26982fbe9b206a902c7a823b0fca7e2e7d709dbc
Josimar Açmeida <Josimar.aalmeida193@gmail.com>
2025-10-12 10:09:50 -0300

    Delete Contagem de números inteiros.py

--- Contagem de números inteiros.py
+++
@@ -1,8 +0,0 @@
-# Contagem de números inteiros
-numero = int(input("Digite um número ineitro para iniciar a contagem: "))
-print("Iniciando contagem...")
-
-for n in range(1,numero + 1):
-    
-    print(f"Número que estou trabalhando agora: {n}")
-print("Contagem concluída! O laço foi finalizado.")
